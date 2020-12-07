bca1da6a99480137eaf7865aebaa44c19ab0a6c8
Maksim Verkhoturov <sd32@protonmail.com>
2020-12-06 23:00:47 -0700

    sequelize init & adjusting the config file to use env vars

---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.DEVUSERNAME,
+    password: process.env.DEVPASSWORD,
+    database: process.env.DEVDB,
+    host: process.env.DEVHOST,
+    port: process.env.DEVPORT,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.TESTUSERNAME,
+    password: process.env.TESTPASSWORD,
+    database: process.env.TESTDB,
+    host: process.env.TESTHOST,
+    port: process.env.TESTPORT,
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.PRODUSERNAME,
+    password: process.env.PRODPASSWORD,
+    database: process.env.PRODDB,
+    host: process.env.PRODHOST,
+    port: process.env.PRODPORT,
+    dialect: 'mysql'
+  }
+};
